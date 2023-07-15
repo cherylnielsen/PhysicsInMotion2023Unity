834f6d7d06051c565996defc749830b844630948
Cheryl R. Nielsen <cnielsen@mail.sfsu.edu>
2023-07-15 09:11:24 -0700

    fix errors in equipment, ramp still broken

--- Assets/Scripts/EquipmentScripts/EquipmentCabinet.cs
+++ Assets/Scripts/EquipmentScripts/EquipmentCabinet.cs
@@ -29,113 +29,79 @@ public class EquipmentCabinet : MonoBehaviour
 {
     [SerializeField] private Button equipmentButton, cartButton, rampButton, sensorButton;
     [SerializeField] private Image equipmentButtonPanel;
-    [SerializeField] private Texture mouseImage;
     [SerializeField] private Camera cam;
     [SerializeField] private GameObject[] equipmentPrefabs;
 
     private enum EquipmentType
     {
-        cart, ramp, sensor
+        cart, ramp, sensor, none
     }
    
     private GameObject newEquipment;
     private bool showCabinet;
+    private EquipmentType equipmentType;
 
+    // Awake is called when the script instance is being loaded
     private void Awake()
     {
         equipmentButtonPanel.gameObject.SetActive(false);
+        equipmentType = EquipmentType.none;
     }
 
     // Start is called before the first frame update
     void Start()
     {
         equipmentButton.onClick.AddListener(ShowEquipmentCabinet);
-        cartButton.onClick.AddListener(delegate { PlaceEquipment(EquipmentType.cart); });
-        rampButton.onClick.AddListener(delegate { PlaceEquipment(EquipmentType.ramp); });
-        sensorButton.onClick.AddListener(delegate { PlaceEquipment(EquipmentType.sensor); });
+        cartButton.onClick.AddListener(delegate { SetEquipmentType(EquipmentType.cart); });
+        rampButton.onClick.AddListener(delegate { SetEquipmentType(EquipmentType.ramp); });
+        sensorButton.onClick.AddListener(delegate { SetEquipmentType(EquipmentType.sensor); });
 
-        // locks the location of the mouse cursor in the center of the screen,
-        // and makes it invisible so that it does not distract.
-        // WARNING: Remember to use Escape Key (ESC) to restore the mouse cursor so you can leave the game.
-        //Debug.Log("WARNING: Remember to use Escape Key to restore the mouse cursor.");
-        //Cursor.lockState = CursorLockMode.Locked;
-        //Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
-    }
-
-    
-    void OnGUI()
-    {
-        // Lets the user know where the ray shooter is currently pointing
-        int size = 20;
-        float posX = cam.pixelWidth / 2 - size / 4;
-        float posY = cam.pixelHeight / 2 - size / 2;
-        GUI.DrawTexture(new Rect(posX, posY, size, size), mouseImage);
-        //GUI.Label(new Rect(posX, posY, size, size), "*");
-
-
-    }
-    
-
-    //void PlaceEquipment(EquipmentType equipType)
-    //{
-        /**
-        Vector3 point = new Vector3();
-        Event currentEvent = Event.current;
-        Vector2 mousePos = new Vector2();
-
-        // Get the mouse position from Event.
-        // Note that the y position from Event is inverted.
-        mousePos.x = currentEvent.mousePosition.x;
-        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
-
-        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-        **/
-        /**
-        // Send a ray into the screen in the direction of the mouse.
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-        // Place the equipment where the ray cast hit.
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        if ((int)equipmentType != (int)EquipmentType.none)
         {
+            PlaceEquipment();
+        }
+
+    }
+    
+    
+    void SetEquipmentType(EquipmentType equipType)
+    {
+        equipmentType = equipType;
+    }
+
+ 
+    void PlaceEquipment()
+    {
+        int equipType = (int)equipmentType;
+        int none = (int)EquipmentType.none;
+
+        // if the mouse has been clicked in the lab room this returns true
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()
+            && equipType != none)
+        {
+            // Send a ray into the screen in the direction of the mouse.
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            // Place the equipment where the ray cast hit.
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
             // get the object that the ray hit
             // GameObject hitObject = hit.transform.gameObject;
             // or use Transform objectHit = hit.transform;
             // Do something with the object that was hit by the raycast.
-            newEquipment = Instantiate(equipmentPrefabs[(int)equipType]);
+            newEquipment = Instantiate(equipmentPrefabs[equipType]);
             newEquipment.transform.position = hit.point;
             Debug.Log("Hit: " + hit.point);
-        }
-
-
-    }**/
-
-
-    
-    void PlaceEquipment(EquipmentType equipType)
-    {
-
-        // Send a ray in the direction that the camera is looking at the center of the camera field of view.
-        Vector3 point = new Vector3(cam.pixelWidth / 2, cam.pixelHeight / 2, 0);
-        Ray ray = cam.ScreenPointToRay(point);
-        RaycastHit hit;
-
-        // If a hit, then place equipment where the ray cast hit.
-        if (Physics.Raycast(ray, out hit))
-        {
-            newEquipment = Instantiate(equipmentPrefabs[(int)equipType]);
-            newEquipment.transform.position = hit.point;
-            Debug.Log("Hit: " + hit.point);
+            }
         }
 
     }
-    
 
 
     void ShowEquipmentCabinet()
@@ -143,6 +109,7 @@ public class EquipmentCabinet : MonoBehaviour
         if(showCabinet)
         {
             equipmentButtonPanel.gameObject.SetActive(false);
+            equipmentType = EquipmentType.none;
             showCabinet = false;
         }
         else
